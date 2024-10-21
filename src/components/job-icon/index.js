d4d257c58b4c65d4b9206016ee6183417fb5c024
unknown <Bgoodluck>
2024-10-21 07:50:24 +0100

    jobmarche

--- src/components/job-icon/index.js
+++ src/components/job-icon/index.js
@@ -1,12 +1,12 @@
 import Image from "next/image";
-import logo from "../../assets/jobmarche.png"
+import jobmarche from "@/assets/images/jobmarche.png";
 
 
 
 function JobIcon() {
     return ( 
         <>
-          <Image src={logo} width={48} height={48} alt="logo" className="rounded-full mb-4"/>
+          <Image src={jobmarche} width={48} height={48} alt="logo" className="rounded-full mb-4"/>
         </>
      );
 }
