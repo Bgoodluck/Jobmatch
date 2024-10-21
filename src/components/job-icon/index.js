fb72be86ca2b1531a30ff800342a3cfe580ef8e7
unknown <Bgoodluck>
2024-10-21 08:20:37 +0100

    public

--- src/components/job-icon/index.js
+++ src/components/job-icon/index.js
@@ -1,14 +1,27 @@
+// import Image from "next/image";
+// import jobmarche from "@/assets/images/jobmarche.png";
+
+
+
+// function JobIcon() {
+//     return ( 
+//         <>
+//           <Image src={jobmarche} width={48} height={48} alt="logo" className="rounded-full mb-4"/>
+//         </>
+//      );
+// }
+
+// export default JobIcon;
+
+
 import Image from "next/image";
-import jobmarche from "@/assets/images/jobmarche.png";
-
-
 
 function JobIcon() {
-    return ( 
-        <>
-          <Image src={jobmarche} width={48} height={48} alt="logo" className="rounded-full mb-4"/>
-        </>
-     );
+  return (
+    <>
+      <Image src="/assets/images/jobmarche.png" width={48} height={48} alt="logo" className="rounded-full mb-4" />
+    </>
+  );
 }
 
 export default JobIcon;
